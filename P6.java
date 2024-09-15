0e936a369270b6f981b2c290f1fc8a72df6dbf1e
katilkorra <katilkorra@gmail.com>
2024-09-16 03:51:34 +0530

    Add files via upload

---
+++ P6.java
@@ -0,0 +1,41 @@
+class Circle {
+
+    private double x, y, r;
+
+    Circle() {
+        x = 10;
+        y = 20;
+        r = 30;
+    }
+
+    //area of circle calculate method
+    public double area() {
+        return Math.PI * r * r;
+    }
+
+    //perimeter of circle calculate method
+    public double circumference() {
+        return 2 * Math.PI * r;
+    }
+
+    //diameter of circle calculate method
+    public double diameter() {
+        return 2 * r;
+    }
+}
+
+public class P6 {
+
+    public static void main(String[] args) {
+        Circle c1= new Circle();
+        //area
+        double areac = c1.area();
+        System.out.println("Area of Circle:=" + areac);
+        //perimeter
+        double perimeter = c1.circumference();
+        System.out.println("Perimeter of Circle:=" + perimeter);
+        //diameter
+        double diameterc = c1.diameter();
+        System.out.println("diameter of Circle:=" + diameterc);
+    }
+}
